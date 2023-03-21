26a1aa9db5ba2123ac44a84f42dcf1452200c0b1
labibi.lg <labibi.lg@gmail.com>
2023-03-21 11:51:07 +0800

    feat: async、await

--- hexschool/nodeJS/promise_async_await/all.js
+++ hexschool/nodeJS/promise_async_await/all.js
@@ -1,16 +1,48 @@
-const checkScore = (name) => {
+function checkScore(name) {
     return new Promise((resolve, reject) => {
         console.log('正在批改作業');
         setTimeout(() => {
             const score = Math.round(Math.random() * 100);
-            resolve({
-                name: name,
-                score: score
-            });
-        }, 2000)
+            if (score >= 20) {
+                resolve({
+                    name: name,
+                    score: score
+                })
+            } else {
+                reject('慘招退學!')
+            }
+        }, 1000)
     })
 }
 
-checkScore('小明')
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error))
+function checkReward(data) {
+    return new Promise((resolve, reject) => {
+        console.log('正在選擇獎品');
+        setTimeout(() => {
+            if (data.score >= 90) {
+                resolve(`${data.name}${data.score}分=>獲得了電影票`);
+            }else if(data.score < 90 && data.score >= 60){
+                resolve(`${data.name}${data.score}分=>獲得了嘉獎`);
+            }else{
+                reject(`${data.name}${data.score}分=>沒有獎品慘朝毒打!`)
+            }
+        }, 1000)
+    })
+}
+
+// checkScore('小明')
+//     .then((data) => checkReward(data))
+//     .then((reward) => console.log(reward))
+//     .catch((error) => console.log(error))
+
+// async、await 寫法
+const init = async function(){
+    try{
+        const score = await checkScore('小明');
+        const reward = await checkReward(score);
+        console.log(reward);
+    }catch(error){
+        console.log(error);
+    }
+}
+init();
